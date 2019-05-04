aeb1543b4f593cc12b7b8da543032737e66d5d28
Alex Williams <hughaw64@gmail.com>
2019-05-04 07:51:54 +0100

    Added more filter urls
    Added some anti-malware lists, and a adguard filter designed for dns based filtering

--- download.py
+++ download.py
@@ -2,9 +2,12 @@ import urllib.request
 urls = [
         'http://winhelp2002.mvps.org/hosts.txt',
 	'https://adaway.org/hosts.txt',
+	'https://easylist-downloads.adblockplus.org/adwarefilters.txt',
+	'https://easylist-downloads.adblockplus.org/malwaredomains_full.txt',
 	'https://easylist.to/easylist/easylist.txt',
 	'https://easylist.to/easylist/easyprivacy.txt',
-        'https://filters.adtidy.org/extension/chromium/filters/14.txt',
+	'https://filters.adtidy.org/extension/chromium/filters/14.txt',
+	'https://filters.adtidy.org/extension/chromium/filters/15.txt',
 	'https://filters.adtidy.org/extension/chromium/filters/2.txt',
 	'https://filters.adtidy.org/extension/chromium/filters/3.txt',
 	'https://github.com/EFForg/badger-sett/blob/master/results.json',
@@ -12,6 +15,7 @@ urls = [
 	'https://hosts-file.net/ad_servers.txt',
 	'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext',
 	'https://raw.githubusercontent.com/AdroitAdorKhan/EnergizedProtection/master/core/hosts',
+	'https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt',
 	'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
 	'https://someonewhocares.org/hosts/hosts',
 	'https://www.malwaredomainlist.com/hostslist/hosts.txt'
