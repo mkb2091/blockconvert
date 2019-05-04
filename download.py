69428960ddbead6e8338cd0a9910bd894cedc3cf
Alex Williams <hughaw64@gmail.com>
2019-05-04 06:51:37 +0100

    Added a few more urls
    Also change useragent to ignore useragent based blocking

--- download.py
+++ download.py
@@ -8,10 +8,15 @@ urls = [
     'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
     'https://someonewhocares.org/hosts/hosts',
     'https://easylist.to/easylist/easylist.txt',
-    'https://easylist.to/easylist/easyprivacy.txt'
+    'https://easylist.to/easylist/easyprivacy.txt',
+    'https://adaway.org/hosts.txt',
+    'https://hosts-file.net/ad_servers.txt',
+    'https://www.malwaredomainlist.com/hostslist/hosts.txt'
     ]
 for (i, url) in enumerate(urls):
-    print('Starting %s' % i)
-    with urllib.request.urlopen(url) as response:
+    print('Starting %s, url: %s' % (i, url))
+    req = urllib.request.Request(url, data=None,
+                                 headers={'User-Agent':'BlockConvert'})
+    with urllib.request.urlopen(req) as response:
         with open('target/%s' % i, 'wb') as file:
             file.write(response.read())
