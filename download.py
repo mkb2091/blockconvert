232777af4bfd0a9454a1d05a65c5921c95b6e693
Alex Williams <hughaw64@gmail.com>
2019-05-05 12:13:42 +0100

    Added anti cryptomining filters

--- download.py
+++ download.py
@@ -24,6 +24,8 @@ urls = [
 	'https://raw.githubusercontent.com/EFForg/badger-sett/master/results.json',
 	'https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt',
 	'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
+        'https://raw.githubusercontent.com/austinheap/sophos-xg-block-lists/master/coinblocker.txt',
+        'https://raw.githubusercontent.com/anudeepND/blacklist/master/CoinMiner.txt',
         'https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/Subscribable-Lists/ParsedBlacklists/Notracking-hostnames.txt',
         'https://raw.githubusercontent.com/hl2guide/All-in-One-Customized-Adblock-List/master/deanoman-adblocklist.txt',
 	'https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt',
