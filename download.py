808f06c28987c6456ee3790f83c626ac6a8ba9bb
Alex Williams <hughaw64@gmail.com>
2019-05-10 07:42:11 +0100

    Added new filter

--- download.py
+++ download.py
@@ -45,6 +45,7 @@ urls = [
     'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt',#GPLv3
     'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt',#GPLv3
     'https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt',#Permission to modify,copy and distribute
+    'https://v.firebog.net/hosts/BillStearns.txt',#GPL
     ]
 
 def copy_whitelist_and_clean():
