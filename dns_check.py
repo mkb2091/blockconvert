312d9337301c333d3a4cf29db6fe15baffa8905b
Alex Williams <hughaw64@gmail.com>
2019-05-13 09:28:41 +0100

    Added reverse dns lookup

--- dns_check.py
+++ dns_check.py
@@ -1,4 +1,6 @@
 import threading
+import ipaddress
+import urllib
 import queue
 import json
 import time
@@ -7,12 +9,14 @@ import os
 import requests
 
 class DNSCheckerWorker(threading.Thread):
-    def __init__(self, session, servers, request_queue, response_queue):
+    def __init__(self, session, servers, request_queue, response_queue,
+                 request_type=1):
         threading.Thread.__init__(self)
         self.session = session
         self.servers = servers
         self.request_queue = request_queue
         self.response_queue = response_queue
+        self.request_type = request_type
     def run(self):
         session = self.session
         servers = self.servers
@@ -21,14 +25,26 @@ class DNSCheckerWorker(threading.Thread):
         pos = 0
         try:
             while True:
-                domain = request_queue.get_nowait()
+                old_domain = domain = request_queue.get_nowait()
+                if self.request_type == 12:
+                    try:
+                        ip = ipaddress.ip_address(domain)
+                    except ValueError:
+                        response_queue.put((old_domain, {})) 
+                    if isinstance(ip, ipaddress.IPv4Address):
+                        domain = ip.exploded.split('.')[::-1]
+                    else:
+                        domain = ipaddress.ip_address(domain).exploded.replace(':', '')[::-1]
+                    domain = '.'.join(domain)
+                    domain += '.in-addr.arpa.'
                 success = False
                 for retry in range(3):
                     pos = (pos + 1) % len(servers)
                     server = servers[pos]
                     try:
-                        r = session.get(server + domain)
-                        response_queue.put((domain, r.json()['Status'] == 0))
+                        r = session.get(server + urllib.parse.urlencode(
+                            {'name':domain, 'type':self.request_type}))
+                        response_queue.put((old_domain, r.json()))
                         if retry > 0:
                             print('Fixed\n', end='')
                         success = True
@@ -46,10 +62,11 @@ class DNSChecker():
         self.session = requests.Session()
         self.session.headers['User-Agent'] = 'DOH'
         self.session.headers['accept'] = 'application/dns-json'
-        self.servers = ['https://dns.google.com/resolve?name=',
-                        'https://cloudflare-dns.com/dns-query?type=1&name=',
-                        'https://doh.securedns.eu/dns-query?name=',]
+        self.servers = ['https://dns.google.com/resolve?',
+                        'https://cloudflare-dns.com/dns-query?',
+                        'https://doh.securedns.eu/dns-query?',]
         self.cache = dict()
+        self.reverse_cache = dict()
         one_week_ago = (time.time() - 60*60*24*7)
         try:
             with open('dns_cache.txt') as file:
@@ -59,6 +76,18 @@ class DNSChecker():
                         last_modified = int(last_modified)
                         if last_modified > one_week_ago:
                             self.cache[domain] = (bool(exists), last_modified)
+                    except (ValueError):
+                        pass
+        except FileNotFoundError:
+            pass
+        try:
+            with open('reverse_dns_cache.txt') as file:
+                for line in file:
+                    try:
+                        ip_address, last_modified, *domains = line.rstrip().split(',')
+                        last_modified = int(last_modified)
+                        if last_modified > one_week_ago:
+                            self.reverse_cache[ip_address] = (last_modified, domains)
                     except (ValueError):
                         pass
         except FileNotFoundError:
@@ -82,7 +111,7 @@ class DNSChecker():
         threads = []
         for i in range(thread_count):
             thread = DNSCheckerWorker(self.session, self.servers, request_queue,
-                                      response_queue)
+                                      response_queue, 1)
             thread.start()
             threads.append(thread)
         start = time.time()
@@ -90,7 +119,8 @@ class DNSChecker():
         while any(thread.is_alive() for thread in threads):
             try:
                 while True:
-                    domain, exists = response_queue.get(timeout=1.0)
+                    domain, result = response_queue.get(timeout=0.1)
+                    exists = result['Status'] == 0
                     results[domain] = exists
                     cache[domain] = (exists, time.time())
                     if len(results) % 2000 == 1999:
@@ -107,3 +137,47 @@ class DNSChecker():
             file.write('\n'.join(','.join(line) for line in lines))
         os.replace('temp', 'dns_cache.txt')
         return results
+    def mass_reverse_lookup(self, ip_list, thread_count=40):
+        domain_list_length = len(ip_list)
+        reverse_cache = self.reverse_cache
+        request_queue = queue.Queue()
+        response_queue = queue.Queue()
+        results = list()
+        all_from_cache = True
+        for ip in ip_list:
+            try:
+                results.extend(reverse_cache[ip][1])
+            except KeyError:
+                request_queue.put(ip)
+                all_from_cache = False
+        if all_from_cache:
+            return results
+        del ip_list
+        threads = []
+        for i in range(thread_count):
+            thread = DNSCheckerWorker(self.session, self.servers, request_queue,
+                                      response_queue, 12)
+            thread.start()
+            threads.append(thread)
+        while any(thread.is_alive() for thread in threads):
+            try:
+                while True:
+                    ip, result = response_queue.get(timeout=0.1)
+                    try:
+                        if result['Status'] == 0:
+                            domains = [answer['data'].lower().strip('.') for answer in result['Answer'] if answer['type'] == 12]
+                            reverse_cache[ip] = (time.time(), domains)
+                            results.extend(domains)
+                        else:
+                            reverse_cache[ip] = (time.time(), [])
+                    except KeyError:
+                        reverse_cache[ip] = (time.time(), [])
+            except queue.Empty:
+                pass
+        lines = [','.join((ip, str(int(reverse_cache[ip][0])), *reverse_cache[ip][1]))
+                 for ip in sorted(reverse_cache)]
+        with open('temp', 'w') as file:
+            file.write('\n'.join(lines))
+        os.replace('temp', 'reverse_dns_cache.txt')
+        return results
+        
