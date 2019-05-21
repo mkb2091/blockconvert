4ac90e45acf78cbf014e768dd769b97dd21f6710
Alex Williams <hughaw64@gmail.com>
2019-05-21 20:34:01 +0100

    Removed many invalid domains from cache

--- blockconvert.py
+++ blockconvert.py
@@ -5,69 +5,19 @@ import os
 import re
 
 import dns_check
+import build_regex
 
 class BlockList():
     def __init__(self, do_dns_check=True, dns_check_threads=40):
         self.blacklist = set()
         self.whitelist = set()
-        self.generate_domain_regex()
-        self.generate_host_regex()
-        self.generate_adblock_regex()
-        self.generate_dns_reponse_policy_zone_regex()
-        self.generate_master_regex()
+        self.REGEX = build_regex.REGEX
+        self.DOMAIN_REGEX = build_regex.DOMAIN_REGEX
+        self.IP_REGEX = build_regex.IP_REGEX
         self.do_dns_check = do_dns_check
         self.dns_check_threads = dns_check_threads
         with open('subdomain_list.txt') as file:
             self.SUBDOMAINS = file.read().splitlines()
-
-    def generate_domain_regex(self):
-        with open('tld_list.txt') as file:
-            self.TLDS = [tld for tld in file.read().lower().splitlines() if '#' not in tld]
-        tlds = self.TLDS.copy()
-        tlds.append(r'*')
-        tld_dict = dict()
-        for tld in tlds:
-            try:
-                tld_dict[tld[0]].append(tld[1:])
-            except KeyError:
-                tld_dict[tld[0]] = [tld[1:]]
-        tld_regex = []
-        for first_letter in sorted(tld_dict, key=lambda x:len(tld_dict[x]), reverse=True):
-            now = '|'.join(['(?:%s)' % re.escape(i) for i in sorted(tld_dict[first_letter], key=len, reverse=True)])
-            tld_regex.append('(?:%s(?:%s))' % (re.escape(first_letter), now))
-        tld_regex = '(?:%s)' % '|'.join(tld_regex)
-        ip_v4 = '[12]?[0-9]{,2}[.][12]?[0-9]{,2}[.][12]?[0-9]{,2}[.][12]?[0-9]{,2}'
-        ip_v6 = '[0-9a-f]{,4}(?:[:][0-9a-f]{,4}){2,8}'
-        ip = '(?:{ip_v4}|{ip_v6})'.format(**locals())
-        self.IP_REGEX = re.compile(ip)
-        segment = r'(?:[a-z0-9_](?:[a-z0-9_-]*[a-z0-9_])?)'
-        self.DOMAIN_STRING =  '[.]?((?:(?:\*[.])?{segment}(?:[.]{segment})*[.]{tld_regex})|{ip})[.]?'.format(**locals())
-        self.DOMAIN_REGEX = re.compile(self.DOMAIN_STRING)
-    def generate_host_regex(self):
-        ips = ['0.0.0.0', '127.0.0.1', '::1']
-        ip_string = '(?:%s)' % '|'.join('(?:%s)' % re.escape(ip) for ip in ips)
-        domain_string = self.DOMAIN_STRING
-        self.HOSTS_STRING = r'{ip_string}\s+{domain_string}\s*(?:\#.*)?'.format(**locals())
-    def generate_adblock_regex(self):
-        domain_string = self.DOMAIN_STRING
-        url_string = r'(?:(?:(?:http(?:s|\*)?)?[:])(?:(?:\/\/)|\*))?{domain_string}\/?'.format(**locals())
-        start = r'(?:\|?\|)?\*?'
-        options = ['popup', r'first\-party', r'\~third\-party', r'third\-party']
-        options_noop = ['important', r'domain\=\2']
-        options_string = '(?:%s)' % '|'.join('(?:%s)' % i for i in options)
-        options_other = '(?:%s)' % '|'.join('(?:%s)' % i for i in ['[a-z~-]+'] + options_noop)
-        options_full = r'\$(?:(?:(?:{options_other}[,])*{options_string}(?:[,]{options_other})*)|%s)'.format(**locals())
-        options_full%= '|'.join('(?:%s)' % i for i in options_noop)
-        ending = r'[*]?\|?\^?(?:{options_full})?(?:\s+(?:(?:\!.*)|(?:\#.*))?)?'.format(**locals())
-        href_element_hiding = r'\#\#\[href\^?\=\"{url_string}\"\]'.format(**locals())
-        self.ADBLOCK_STRING = r'(?:{start}{url_string}{ending})|(?:{href_element_hiding})'.format(**locals())
-    def generate_dns_reponse_policy_zone_regex(self):
-        domain_string = self.DOMAIN_STRING
-        self.RPZ_STRING = r'{domain_string}\s+cname\s+[.]\s*(?:[;].*)?'.format(**locals())
-        
-    def generate_master_regex(self):
-        self.REGEX_STRING = '(?:%s)|(?:%s)|(?:%s)' % (self.HOSTS_STRING, self.ADBLOCK_STRING, self.RPZ_STRING)
-        self.REGEX = re.compile(self.REGEX_STRING)
 
     def add_file(self, contents, is_whitelist=False):
         data = contents.lower()
