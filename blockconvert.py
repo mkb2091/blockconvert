4b53a1773184ccab820f88a78b7e982e0dac526d
Alex Williams <hughaw64@gmail.com>
2019-05-21 15:55:14 +0100

    Ran update, and fixed bug

--- blockconvert.py
+++ blockconvert.py
@@ -46,7 +46,7 @@ class BlockList():
         ips = ['0.0.0.0', '127.0.0.1', '::1']
         ip_string = '(?:%s)' % '|'.join('(?:%s)' % re.escape(ip) for ip in ips)
         domain_string = self.DOMAIN_STRING
-        self.HOSTS_STRING = r'(?:{ip_string}\s+)?{domain_string}\s*(?:\#.*)?'.format(**locals())
+        self.HOSTS_STRING = r'{ip_string}\s+{domain_string}\s*(?:\#.*)?'.format(**locals())
     def generate_adblock_regex(self):
         domain_string = self.DOMAIN_STRING
         url_string = r'(?:(?:(?:http(?:s|\*)?)?[:])(?:(?:\/\/)|\*))?{domain_string}\/?'.format(**locals())
@@ -57,7 +57,7 @@ class BlockList():
         options_other = '(?:%s)' % '|'.join('(?:%s)' % i for i in ['[a-z~-]+'] + options_noop)
         options_full = r'\$(?:(?:(?:{options_other}[,])*{options_string}(?:[,]{options_other})*)|%s)'.format(**locals())
         options_full%= '|'.join('(?:%s)' % i for i in options_noop)
-        ending = r'[*]?\|?\^?(?:{options_full})?\s*(?:\!.*)?'.format(**locals())
+        ending = r'[*]?\|?\^?(?:{options_full})?(?:\s+(?:(?:\!.*)|(?:\#.*))?)?'.format(**locals())
         href_element_hiding = r'\#\#\[href\^?\=\"{url_string}\"\]'.format(**locals())
         self.ADBLOCK_STRING = r'(?:{start}{url_string}{ending})|(?:{href_element_hiding})'.format(**locals())
     def generate_master_regex(self):
@@ -155,7 +155,7 @@ class BlockList():
         for filter_list in [self.blacklist, self.whitelist]:
             for url in list(filter_list):
                 if not self.REGEX.match(url):
-                    print('Removing', url)
+                    print('Removing: %s' % url)
                     filter_list.remove(url)
     def to_domain_list(self):
         return '\n'.join(sorted(self.blacklist))
