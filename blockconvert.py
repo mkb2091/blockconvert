d2379758c6ce7edc56ef2c031479d55178abc043
Alex Williams <hughaw64@gmail.com>
2019-05-23 09:17:31 +0100

    Optimized slightly

--- blockconvert.py
+++ blockconvert.py
@@ -8,17 +8,17 @@ import dns_check
 import build_regex
 
 class BlockList():
-    def __init__(self, do_dns_check=True, dns_check_threads=40):
+    def __init__(self, dns_check_threads=40):
         self.blacklist = set()
         self.whitelist = set()
         self.REGEX = build_regex.REGEX
         self.DOMAIN_REGEX = build_regex.DOMAIN_REGEX
         self.IP_REGEX = build_regex.IP_REGEX
         self.TLDS = build_regex.TLDS
-        self.do_dns_check = do_dns_check
-        self.dns_check_threads = dns_check_threads
+        self.dns_check_threads = max(1, dns_check_threads)
         with open('subdomain_list.txt') as file:
             self.SUBDOMAINS = file.read().splitlines()
+        self.dns = dns_check.DNSChecker()
 
     def add_file(self, contents, is_whitelist=False):
         data = contents.lower()
@@ -55,41 +55,52 @@ class BlockList():
                     elif data['action_map'][i]['heuristicaction'] == 'cookieblock':
                         self.whitelist.add(i)
     def clean(self, is_malware=False):
-        dns = dns_check.DNSChecker()
+        dns = self.dns
         last = time.time()
         for filter_list in [self.blacklist, self.whitelist]:
             ips = []
-            for item in list(filter_list):
+            for item in filter_list:
                 if self.IP_REGEX.fullmatch(item):
                     ips.append(item)
-                    filter_list.remove(item)
             if ips:
+                for ip in ips:
+                    filter_list.remove(ip)
                 found = dns.mass_reverse_lookup(ips)
                 filter_list.update(found)
-                print('Added %s rules via reverse dns(%ss)' % (len(found), time.time() - last))
+                print('Added %s rules via reverse dns' % len(found))
+        print('Checked for IP addresses(%ss)' % (time.time() - last))
         last = time.time()
         print('Started with %s rules' % len(self.blacklist))
         for filter_list in [self.blacklist, self.whitelist]:
-            for url in list(filter_list):
+            star_tld = []
+            for url in filter_list:
                 if url.endswith('*'):
-                    filter_list.remove(url)
-                    for tld in self.TLDS:
-                        filter_list.add(url[:-1]+tld)
+                    star_tld.append(url)
+            for url in star_tld:
+                filter_list.remove(url)
+                for tld in self.TLDS:
+                    filter_list.add(url[:-1]+tld)
         print('Expanded .* TLD to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
         last = time.time()
         for filter_list in [self.blacklist, self.whitelist]:
-            for url in list(filter_list):
+            to_remove_subdomains = []
+            for url in filter_list:
                 if url.split('.')[0] in ('m', 'www'):
-                    filter_list.add('.'.join(url.split('.')[1:]))
+                    to_remove_subdomains.append(url)
+            for url in to_remove_subdomains:
+                filter_list.add('.'.join(url.split('.')[1:]))
         print('Expanded to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
         last = time.time()
         for filter_list in [self.blacklist, self.whitelist]:
-            for url in list(filter_list):
+            star_subdomain = []
+            for url in filter_list:
                 if url.startswith('*.'):
-                    filter_list.remove(url)
-                    filter_list.add(url[2:])
-                    for subdomain in self.SUBDOMAINS:
-                        filter_list.add(subdomain + '.' + url[2:])
+                    star_subdomain.append(url)
+            for url in star_subdomain:
+                filter_list.remove(url)
+                filter_list.add(url[2:])
+                for subdomain in self.SUBDOMAINS:
+                    filter_list.add(subdomain + '.' + url[2:])
         print('Expanded *. subdomain to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
         last = time.time()
         for i in self.whitelist:
@@ -99,15 +110,13 @@ class BlockList():
                 pass
         print('Cleaned to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
         last = time.time()
-        if self.do_dns_check:
-            last = time.time()
-            result = dns.mass_check(self.blacklist, self.dns_check_threads)
-            print('Performed lookups(%ss)' % (time.time() - last))
-            last = time.time()
-            for domain in result:
-                if not result[domain]:
-                    self.blacklist.remove(domain)
-            print('Trimmed to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
+        result = dns.mass_check(self.blacklist, self.dns_check_threads)
+        print('Performed lookups(%ss)' % (time.time() - last))
+        last = time.time()
+        for domain in result:
+            if not result[domain]:
+                self.blacklist.remove(domain)
+        print('Trimmed to %s rules(%ss)' % (len(self.blacklist), time.time() - last))
         last = time.time()
         for filter_list in [self.blacklist, self.whitelist]:
             for url in list(filter_list):
