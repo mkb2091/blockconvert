3dc946acc3940efff851300f86a3c0deaf8f2e18
Alex Williams <hughaw64@gmail.com>
2019-06-16 08:12:28 +0100

    Improved README.md

--- generate_readme.py
+++ generate_readme.py
@@ -1,7 +1,7 @@
 TITLE = '# BlockConvert'
 
 DESCRIPTION = '''
-Generated blocklist in a variety of formats.
+Malware, advert and tracking blacklist
 
 Advantages of using this list:
 - Conversion of list types. As well as supporting many common filter list formats, \
@@ -24,6 +24,35 @@ blocked), then please make an issue and I will remove it)
 - Use of DNS to check if domains still exist. Many lists contain domains that \
 have expired and no longer exist. This makes those lists larger than needed \
 which wastes bandwidth, space and can slow blocking.
+
+##What this blocks:
+- Malware/Phishing: Many malware lists are used in building this list, including \
+multiple malware IP lists, which are used to find many more malware domains.
+
+- Adverts: Adblock syntax is partially supported, so this list is able to extract \
+some advert domains. This list is pretty good at blocking adverts, but an \
+in-browser adblocker such as uBlock Origin is recommended as well as relying on \
+hosts/dns blocking.
+
+- Trackers: Many tracking domains are extracted from the lists used, including \
+Privacy Badger date files which automatically identify trackers.
+
+- Coin mining: A few coin mining blocklists are used to block browser-based \
+coin mining from using cpu.
+
+##How to use:
+- Pi-hole: Go to web interface. Log in. Settings -> Blocklists. Copy domain list \
+url(Pi-hole supports hosts and domain lists, and domain list is a smaller file) \
+from below in the links section, and paste it in the textbox. Click Save.
+
+- Blokada: Open Blokada. Click shield with black middle which says "{{number}} in \
+blacklist". Click plus in circle at bottom of screen. Copy and paste hosts file \
+from link sections. Click save
+
+- uBlock Origin: Click uBlock Origin logo. Click open dashboard(3 horizontal \
+lines, under disable uBlock Origin button, on the right). Click Filter lists. \
+Scroll to bottom, and click Import(in custom section). Copy and paste either \
+adblock style blocklist or domain blocklist from the link section below.
 '''
 
 PROCESS = '\n\n'.join((
