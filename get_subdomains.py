5ca109d14f885365dbe42eb2774eb554c3a72c58
Alex Williams <hughaw64@gmail.com>
2019-07-08 18:56:33 +0100

    Fixed major false positive

--- get_subdomains.py
+++ get_subdomains.py
@@ -11,10 +11,13 @@ def get_subdomains(dns_checker, domains):
     reverse_lookup_results = dns_checker.mass_reverse_lookup(ip_set)
     results = set()
     targets = []
+    min_length = len(domains[0])
     for target in domains:
         targets.append('.' + target)
+        if len(target)< min_length:
+            min_length = len(target)
     for domain in dns_checker.cache:
-        if dns_checker.cache[domain]:
+        if len(domain) >= (min_length + 2) and dns_checker.cache[domain]:
             for target in targets:
                 if domain.endswith(target):
                     results.add(domain)
--- sources.md
+++ sources.md
@@ -1,6 +1,6 @@
 ## Last Commit Infomation
 
-Theres are 1036326 blocked domains in each of the generated filter lists
+Theres are 1036229 blocked domains in each of the generated filter lists
 
 ## Sources
 
