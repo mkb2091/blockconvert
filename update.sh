500f8c2e048eafd6592272641a130e1b16b1e17c
Alex Williams <hughaw64@gmail.com>
2020-02-19 12:03:22 +0000

    Reduced issues with update script

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 set -e
+git checkout -- .
 git pull
 python3 main.py
+git pull
 git commit -a -m "Ran Quick Autoupdate"
 git push
 python3 main.py -u
+git pull
 git commit -a -m "Ran Full Autoupdate"
 git push
