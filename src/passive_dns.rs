1d093f7276e7e4e5307ff36d7b9da0831311b9a3
Alex Williams <hughaw64@gmail.com>
2020-07-19 17:22:32 +0100

    Added virustotal

--- src/passive_dns.rs
+++ src/passive_dns.rs
@@ -176,8 +176,10 @@ pub async fn threatminer_passive_dns() -> Result<(), Box<dyn std::error::Error>>
             .await
         {
             if let Ok(json) = response.json::<serde_json::Value>().await {
-                if json.pointer("/status_code") != Some(&serde_json::Value::String("200".to_string()))
-                    && json.pointer("/status_code") != Some(&serde_json::Value::String("404".to_string()))
+                if json.pointer("/status_code")
+                    != Some(&serde_json::Value::String("200".to_string()))
+                    && json.pointer("/status_code")
+                        != Some(&serde_json::Value::String("404".to_string()))
                 {
                     println!(
                         "THREATMINER: Non 200 response code: {:?}",
@@ -198,7 +200,7 @@ pub async fn threatminer_passive_dns() -> Result<(), Box<dyn std::error::Error>>
                         }
                     }
                 } else {
-                    println!("No data field: {:?}", json);
+                    println!("THREATMINER: No data field: {:?}", json);
                     errors += 1;
                 }
             } else {
@@ -226,6 +228,110 @@ pub async fn threatminer_passive_dns() -> Result<(), Box<dyn std::error::Error>>
             db.flush().await?;
             wtr.flush().await?;
             last_flushed = std::time::Instant::now();
+        }
+    }
+    db.flush().await?;
+    wtr.flush().await?;
+    Ok(())
+}
+
+pub async fn virus_total_passive_dns(key: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut ips = get_ips().await?;
+
+    let mut path = std::path::PathBuf::from(PASSIVE_DNS_RECORD_DIR);
+    path.push("virustotal");
+    let mut db = DirectoryDB::new(&path).await?;
+    db.read(|line| {
+        if let Ok(ip) = line.trim().parse::<std::net::IpAddr>() {
+            ips.remove(&ip);
+        }
+    })
+    .await?;
+    let total_length = ips.len();
+    println!("VIRUSTOTAL: {} ips remaining", total_length);
+    let client = reqwest::Client::new();
+    let mut ips_checked: u64 = 0;
+    let mut domains_found: u64 = 0;
+    let mut errors: u64 = 0;
+
+    let _ = std::fs::create_dir(EXTRACTED_DOMAINS_DIR);
+    let mut path = std::path::PathBuf::from(EXTRACTED_DOMAINS_DIR);
+    path.push(std::path::PathBuf::from(format!(
+        "virustotal_{:?}",
+        chrono::Utc::today()
+    )));
+    let mut wtr = BufWriter::new(
+        OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(path)
+            .await?,
+    );
+    let now = std::time::Instant::now();
+    let mut last_flushed = std::time::Instant::now();
+    for ip in ips.into_iter() {
+        ips_checked += 1;
+        if let Ok(response) = client
+            .get("https://www.virustotal.com/vtapi/v2/ip-address/report")
+            .query(&[("apikey", &key), ("ip", &ip.to_string())])
+            .send()
+            .await
+        {
+            /*println!("response: {:?}", response.text().await?);
+            loop {};*/
+            if let Ok(json) = response.json::<serde_json::Value>().await {
+                if json.pointer("/response_code")
+                    != Some(&serde_json::Value::Number(serde_json::Number::from(1)))
+                {
+                    println!(
+                        "VIRUSTOTAL: Non 1 response code: {:?}",
+                        json.pointer("/status_code")
+                    );
+                    return Err(Box::new(InvalidResponseCode::default()));
+                }
+                if let Some(data) = json
+                    .pointer("/resolutions")
+                    .and_then(|data| data.as_array())
+                {
+                    for domain in data.iter().filter_map(|item| {
+                        item.pointer("/hostname").and_then(|domain| domain.as_str())
+                    }) {
+                        if let Ok(domain) = domain.parse::<Domain>() {
+                            domains_found += 1;
+                            wtr.write_all(domain.to_string().as_bytes()).await?;
+                            wtr.write_all(b"\n").await?;
+                        } else {
+                            println!("VIRUSTOTAL: Failed to parse: {}", domain)
+                        }
+                    }
+                } else {
+                    println!("VIRUSTOTAL: No data field: {:?}", json);
+                    errors += 1;
+                }
+            } else {
+                println!("VIRUSTOTAL: Failed to parse as json");
+                errors += 1;
+            }
+        } else {
+            println!("VIRUSTOTAL: Connection failed");
+            errors += 1;
+            async_std::task::sleep(std::time::Duration::from_secs(15_u64)).await
+        }
+        async_std::task::sleep(std::time::Duration::from_secs(15_u64)).await;
+
+        db.write_line(ip.to_string().as_bytes()).await?;
+        if last_flushed.elapsed().as_secs() > 10 {
+            db.flush().await?;
+            wtr.flush().await?;
+            last_flushed = std::time::Instant::now();
+            println!(
+                "VIRUSTOTAL: Checked {}/{} ips ({}/s), found {} domains with {} errors",
+                ips_checked,
+                total_length,
+                (ips_checked as f32 / now.elapsed().as_secs_f32()),
+                domains_found,
+                errors
+            );
         }
     }
     db.flush().await?;
