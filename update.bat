97f8534ea4eebf1f3d028685fc7efd815829eb20
Alex Williams <mkb2191@protonmail.com>
2021-06-24 14:19:33 +0100

    Removed unneeded args

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-cargo run --release generate --concurrent-requests 10 --dns-max-age 1209600 --extracted-max-age 604800 --file-max-size 40000000
+cargo run --release generate
 git commit output -m "Ran Autoupdate"
 git push
