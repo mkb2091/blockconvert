97395112ecec63858d4f295d2796edc947d566c0
Alex Williams <mkb2191@protonmail.com>
2021-06-25 18:46:56 +0100

    Improved errors for DoH

--- src/doh.rs
+++ src/doh.rs
@@ -39,31 +39,65 @@ struct DoHResult {
     comment: String,
 }
 
+#[derive(Debug)]
+pub enum LookupError {
+    Request(reqwest::Error),
+    Parsing(serde_json::Error),
+}
+
+impl From<reqwest::Error> for LookupError {
+    fn from(error: reqwest::Error) -> Self {
+        Self::Request(error)
+    }
+}
+
+impl From<serde_json::Error> for LookupError {
+    fn from(error: serde_json::Error) -> Self {
+        Self::Parsing(error)
+    }
+}
+
+impl std::fmt::Display for LookupError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Request(io) => write!(f, "DOH Request Error: {}", io),
+            Self::Parsing(parsing) => write!(f, "DOH Parsing Error: {}", parsing),
+        }
+    }
+}
+
+impl std::error::Error for LookupError {}
+
 async fn lookup_domain_(
     server: Arc<str>,
     client: &reqwest::Client,
     timeout: Option<std::time::Duration>,
     domain: &Domain,
-) -> Result<Option<DNSResultRecord>, reqwest::Error> {
+) -> Result<Option<DNSResultRecord>, LookupError> {
     let mut req = client
         .get(&*server)
         .query(&[("name", &**domain), ("type", "1")]);
     if let Some(timeout) = timeout {
         req = req.timeout(timeout);
     }
-    let json: DoHResult = req
+
+    let data = req
         .send()
         .await
         .map_err(|err| {
             println!("Error: {:?}", err);
             err
         })?
-        .json()
+        .text()
         .await
         .map_err(|err| {
             println!("Error: {:?}", err);
             err
         })?;
+    let json: DoHResult = serde_json::from_str(&data).map_err(|error| {
+        println!("Error: {:?}\nWith data:  {:?}", error, data);
+        error
+    })?;
     if json.Status == 0 {
         let mut result = DNSResultRecord {
             domain: domain.clone(),
@@ -100,7 +134,7 @@ pub async fn lookup_domain(
     attempts: usize,
     timeout: Option<std::time::Duration>,
     domain: &Domain,
-) -> Result<Option<DNSResultRecord>, reqwest::Error> {
+) -> Result<Option<DNSResultRecord>, LookupError> {
     for _ in 0..attempts {
         if let Ok(result) = lookup_domain_(server.clone(), &client, timeout, domain).await {
             return Ok(result);
